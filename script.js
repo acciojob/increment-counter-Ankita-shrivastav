b3cf450b9fef96b136d99fa44e364d75c27db368
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-13 13:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-//your JS code here. If required.
- let button =document.getElementById("incrementBtn");
- button.addEventListener("Click",increasedtheValue);
+your JS code here. If required.
+//  let button =document.querySelector("#incrementBtn");
+//  button.addEventListener("click",increasedtheValue);
 
-function increasedtheValue(){
-	let counter=document.getElementById("counter");
-	let counter2=Number(counter.innerHTMl);
-	counter2=+1;
-	counter.innerHtML=counter2;
-	alert(counter2-2);
+// function increasedtheValue(){
+// 	alert('hi');
+// 	let counter=document.getElementById("counter");
+// 	let counter2=Number(counter.innerHTMl);
+// 	counter2=+1;
+// 	counter.innerHtML="counter2";
+// 	alert(counter2-2);
+// }
+//  let counter=document.getElementById("counter");
+//    let incrementBtn=document.get ElementById("incrementBtn");
+//    let counter=0;
+
+function incrementCounter(){
+	alert("Un-incremented value:" count++);
+	counter.text1=count;
 }
+incrementBtn.addEventListener("click",incrementCounter);
+
+
+
+
