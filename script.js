17f91a64ddd4c87f7b2eb830d77428c901a35c12
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-13 13:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ let button =document.getElementById("incrementBtn");
+ button.addEventListener("Click",increasedtheValue);
+
+function increasedtheValue(){
+	let counter=document.getElementById("counter");
+	let counter2=Number(counter.innerHTMl);
+	counter2=+1;
+	counter.innerHtML=counter2;
+	alert(counter2-2);
+}
